1f5d4136ab43cc5d667d6c0b182ee26ddd0a658f
OfficialAnubrata <anubrata.sarkar@iemlabs.com>
2025-05-29 11:07:28 +0530

    Adds utility to send emails
    Adds a utility function to send emails using Nodemailer.
    
    This function configures a transporter with Gmail SMTP settings
    and sends an email with the provided subject and HTML content
    to the specified recipient.

---
+++ src/utils/sendEmail.utils.js
@@ -0,0 +1,26 @@
+import { createTransport } from "nodemailer";
+import variable from "../config/env.config.js";
+require("dotenv").config();
+export async function sendEmail(email, subject, html) {
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    tls: {
+        rejectUnauthorized: false,
+        minVersion: "TLSv1.2"
+    },
+    auth: {
+      user: variable.gmail,
+      pass: variable.gmail_password
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.NODEMAILER_GMAIL,
+    to: email,
+    subject: subject,
+    html: html,
+  };
+
+  await transporter.sendMail(mailOptions);
+}
