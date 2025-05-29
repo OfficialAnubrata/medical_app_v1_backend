429d1b3d4d9541602d60e8bfa6826865fd35c22c
OfficialAnubrata <anubrata.sarkar@iemlabs.com>
2025-05-29 11:07:13 +0530

    Adds Gmail configuration to environment variables
    Adds configuration options for Gmail integration using Nodemailer.
    
    This allows the application to securely access Gmail credentials
    from environment variables, enhancing security and configuration
    flexibility.

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -5,7 +5,9 @@ dotenv.config();
 const variable = {
     database_url: process.env.DATABASE_URL, // use all-caps with underscores (best practice)
     jwt_secret: process.env.JWT_SECRET,
-    expery_time: process.env.JWT_EXPIRY_TIME
+    expery_time: process.env.JWT_EXPIRY_TIME,
+    gmail: process.env.NODEMAILER_GMAIL,
+    gmail_password: process.env.NODEMAILER_GMAIL_PASS
 };
 
 export default variable;
