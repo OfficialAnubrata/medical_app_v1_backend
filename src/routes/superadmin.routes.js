417f6d357c4f2311d2e26e085785fe8ce43ce048
OfficialAnubrata <anubrata.sarkar@iemlabs.com>
2025-08-14 14:33:45 +0530

    route updates

--- src/routes/superadmin.routes.js
+++ src/routes/superadmin.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 import superadminBookingController from "../controllers/superadmin.booking.controller.js";
 import { superadminChecker } from "../middlewares/authchecker.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 router.post('/allorders',superadminChecker,superadminBookingController.allorderfromsuperadmin)
-router.post('/changeteststatus/:booking_id',superadminChecker,superadminBookingController.changeteststatusbysuperadmin)
+router.post('/changeteststatus/:booking_id',superadminChecker,upload.single('reportFile'),superadminBookingController.changeteststatusbysuperadmin)
 
 export default router;
